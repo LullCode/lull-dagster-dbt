c9c8676dabf1036e5a231c357b5b321e759ecc19
Jyou <jyou@lull.com>
2022-03-18 10:48:58 -0700

    add class properties
    added environment id, account id, project id

--- lull_dagster_dbt/resources/resources.py
+++ lull_dagster_dbt/resources/resources.py
@@ -5,4 +5,9 @@ from lull_dagster_dbt.src.dbt_api import DBTApi
 
 @resource
 def dbt_interface(init_context):
-    return DBTApi(os.environ.get("DBT_ACCESS_TOKEN"))
+    return DBTApi(
+        access_token=os.environ.get("DBT_ACCESS_TOKEN"),
+        environment_id=os.environ.get("DBT_ENVIRONMENT_ID"),
+        account_id=os.environ.get("DBT_ACCOUNT_ID"),
+        project_id=os.environ.get("DBT_PROJECT_ID"),
+    )
